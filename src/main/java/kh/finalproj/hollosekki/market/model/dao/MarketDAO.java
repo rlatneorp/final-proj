5e904d5e8232620ab6f51e10b898fab7fb2b56c3
rlatneorp <rlatneorp@gmail.com>
2023-06-14 15:59:57 +0900

    출석체크 완료....

--- src/main/java/kh/finalproj/hollosekki/market/model/dao/MarketDAO.java
+++ src/main/java/kh/finalproj/hollosekki/market/model/dao/MarketDAO.java
@@ -2,12 +2,15 @@ package kh.finalproj.hollosekki.market.model.dao;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import kh.finalproj.hollosekki.enroll.model.vo.Users;
+import kh.finalproj.hollosekki.market.model.vo.Attendance;
 import kh.finalproj.hollosekki.market.model.vo.Cart;
 import kh.finalproj.hollosekki.market.model.vo.Product;
 import java.util.ArrayList;
@@ -15,7 +18,6 @@ import java.util.ArrayList;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
-import kh.finalproj.hollosekki.market.model.vo.ShippingAddress;
 
 
 @Repository
@@ -37,17 +39,31 @@ public class MarketDAO {
 		sqlSession.selectOne("marketMapper.firstAdDay", map);
 	}
 
-//	public int aDateCheck(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
-//		return sqlSession.update("marketMapper.aDateCheck", map);
-//	}
+	public int aDateCheck(SqlSessionTemplate sqlSession, HashMap<String, Object> map) {
+		return sqlSession.update("marketMapper.aDateCheck", map);
+	}
 
 	public void checkDay(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
 		sqlSession.selectOne("marketMapper.checkDay", map);
 	}
 
-	public ArrayList<Users> allAt(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
-		return (ArrayList)sqlSession.selectList("marketMapper.allAt", map);
+	public ArrayList<Attendance> allAt(SqlSessionTemplate sqlSession, Users u) {
+		return (ArrayList)sqlSession.selectList("marketMapper.allAt", u);
 	}
+
+	public void atInsert(SqlSessionTemplate sqlSession, HashMap<String, Object> map) {
+		sqlSession.insert("marketMapper.atInsert", map);
+	}
+
+	public void gettedPoint(SqlSessionTemplate sqlSession, HashMap<String, Object> map) {
+		sqlSession.update("marketMapper.gettedPoint", map);
+	}
+
+	public int atTodayChecked(SqlSessionTemplate sqlSession, Users u) {
+		return sqlSession.selectOne("marketMapper.atTodayChecked", u);
+	}
+
+
 
 
 	public int insertShipping(SqlSessionTemplate sqlSession, ShippingAddress sa) {

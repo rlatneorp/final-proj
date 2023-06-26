4f04e461517cc38a75188365ef6bf72db4a56d43
rlatneorp <rlatneorp@gmail.com>
2023-06-26 17:51:10 +0900

    게시판 디테일보기, 댓글창, 댓글...

--- src/main/java/kh/finalproj/hollosekki/board/model/dao/BoardDAO.java
+++ src/main/java/kh/finalproj/hollosekki/board/model/dao/BoardDAO.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import kh.finalproj.hollosekki.board.model.vo.Board;
 import kh.finalproj.hollosekki.common.model.vo.PageInfo;
+import kh.finalproj.hollosekki.market.model.vo.Review;
 
 @Repository
 public class BoardDAO {
@@ -20,6 +21,18 @@ public class BoardDAO {
 
 	public int getFreeBoardListCount(SqlSessionTemplate sqlSession, int i) {
 		return sqlSession.selectOne("boardMapper.getFreeBoardListCount", i);
+	}
+
+	public int addCount(SqlSessionTemplate sqlSession, int bId) {
+		return sqlSession.update("boardMapper.addCount", bId);
+	}
+
+	public Board selectBoard(SqlSessionTemplate sqlSession, int bId) {
+		return sqlSession.selectOne("boardMapper.selectBoard", bId);
+	}
+
+	public void insertReply(SqlSessionTemplate sqlSession, Review r) {
+		sqlSession.insert("boardMapper.insertReply",r);
 	}
 	
 }

f435bfb4d89fc6f55a6803201558625d474d3daf
박보서 <bbosioy@naver.com>
2023-07-05 15:25:43 +0900

    홈 수정중

--- src/main/java/kh/finalproj/hollosekki/HomeController.java
+++ src/main/java/kh/finalproj/hollosekki/HomeController.java
@@ -1,6 +1,7 @@
 package kh.finalproj.hollosekki;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import kh.finalproj.hollosekki.common.model.vo.Image;
 import kh.finalproj.hollosekki.enroll.model.service.EnrollService;
 import kh.finalproj.hollosekki.enroll.model.vo.Users;
+import kh.finalproj.hollosekki.market.model.service.MarketService;
+import kh.finalproj.hollosekki.market.model.vo.Product;
 import kh.finalproj.hollosekki.users.model.service.UsersService;
 
 /**
@@ -33,6 +36,9 @@ public class HomeController {
 	@Autowired
 	private EnrollService eService;
 	
+	@Autowired
+	private MarketService mkService;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -45,11 +51,26 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		Users loginUsers = (Users)model.getAttribute("loginUser");
+		
+		//잘 나가는 상품 조회 
+		ArrayList<Product> likeOrderBy = mkService.selectLikeOrderBy();
+		System.out.println("likeOrderBy : " + likeOrderBy);
+		model.addAttribute("likeOrderBy", likeOrderBy);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		if(loginUsers != null) {
 			Users u = (Users)model.getAttribute("loginUser");
 			Users loginUser = eService.login(u);
 			model.addAttribute("loginUser", loginUser);
-			
 			if(loginUser != null) {
 				Image image = uService.selectImage(loginUser.getUsersNo());
 				model.addAttribute("image", image);
@@ -57,8 +78,6 @@ public class HomeController {
 				Users user = eService.login2(u);
 				model.addAttribute("loginUser", user);
 			}
-			
-			
 			return "home";
 		} else {
 			model.addAttribute("serverTime", formattedDate );
